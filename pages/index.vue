e4be732863dbadf1239b7922d13fa2c377d9f75e
indraarsy <userindra29@gmail.com>
2020-08-25 20:38:04 +0700

    indexnya ilang

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.$auth.loggedIn == true
+      ? this.$router.push('/admin')
+      : this.$router.push('/login')
+  }
+}
+</script>
+
+<style scoped></style>
